5f7dc98c595d2a38423ad4fc263852b0ff20c5b5
Lilac <lyliowy@gmail.com>
2022-11-17 16:47:46 +0000

    Fix markdown formatting & add per-format permissions

--- src/main/java/dev/rosewood/rosechat/message/wrapper/tokenizer/format/FormatTokenizer.java
+++ src/main/java/dev/rosewood/rosechat/message/wrapper/tokenizer/format/FormatTokenizer.java
@@ -17,9 +17,26 @@ public class FormatTokenizer implements Tokenizer<FormatToken> {
             String content = input.substring(0, matcher.end());
             char formatCharacter = content.charAt(1);
             char formatCharacterLowercase = Character.toLowerCase(formatCharacter);
-            boolean hasPermission = this.hasPermission(messageWrapper, ignorePermissions, formatCharacterLowercase == 'k' ? "rosechat.magic" : "rosechat.format");
+            boolean hasPermission = this.hasPermission(messageWrapper, ignorePermissions, this.getPermissionForFormat(formatCharacterLowercase));
             return hasPermission ? new FormatToken(content, ChatColor.getByChar(formatCharacterLowercase), Character.isLowerCase(formatCharacter)) : new FormatToken(content, null);
         }
+        return null;
+    }
+
+    public String getPermissionForFormat(char format) {
+        switch (format) {
+            case 'l':
+                return "rosechat.bold";
+            case 'n':
+                return "rosechat.underline";
+            case 'm':
+                return "rosechat.strikethrough";
+            case 'o':
+                return "rosechat.italic";
+            case 'k':
+                return "rosechat.magic";
+        }
+
         return null;
     }
 
