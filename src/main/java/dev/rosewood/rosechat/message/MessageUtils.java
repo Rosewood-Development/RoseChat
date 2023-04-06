7f0789a783794d8e307139774193f54332c5799f
Lilac <lyliowy@gmail.com>
2023-04-06 18:03:09 +0100

    Fix messages not appearing for the sender

--- src/main/java/dev/rosewood/rosechat/message/MessageUtils.java
+++ src/main/java/dev/rosewood/rosechat/message/MessageUtils.java
@@ -265,7 +265,7 @@ public class MessageUtils {
             target.spigot().sendMessage(receivedMessage);
         }
 
-        if (sender.isConsole() && sender.getName().equalsIgnoreCase("Console")) sender.send(sentMessage);
+        sender.send(sentMessage);
 
         if (Setting.UPDATE_DISPLAY_NAMES.getBoolean() && sender.isPlayer()
                 && !sender.getDisplayName().equals(sender.getNickname())) NicknameCommand.setDisplayName(sender.asPlayer(), sender.getNickname());
